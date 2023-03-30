a626ecf2e84cb7b36096e36521ef8ce59688a670
SkyBeal <thesnugpug1@gmail.com>
2023-03-30 15:02:19 -0500

    Revert "Sword Rotate"
    This reverts commit af6a737e5515c057e1a2dd71198855078d40673f.

--- Assets/Scripts/GlobbingtonAttackController.cs
+++ Assets/Scripts/GlobbingtonAttackController.cs
@@ -2,79 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-/*******************************************************************************
-// File Name :         GlobbingtonAttackController.cs
-// Author(s) :         Sky Beal
-// Creation Date :     3/28/2023
-//
-// Brief Description : Code for Globbington swinging his sword! Also accounts 
-//                     for his movement.
-*****************************************************************************/
 
-public class GlobbingtonAttackController : PlayerController
+public class GlobbingtonAttackController : MonoBehaviour
 {
-    [Header("Settings")]
+    [Header("Fucking Around And Finding Out")]
+    public GameObject Sword;
+    public PlayerInput MyPlayerInput;
+
+    public InputAction Strike;
+
+    private Rigidbody2D myRB;
     public float AttackLength;
 
-    [Header("Unity Stuff")]
-    public GameObject Sword;
-    public InputAction Strike;
-    public Transform RotatePoint;
-    private Rigidbody2D myRB;
-
-   /// <summary>
-   /// steals start from playercontoller and adapts it for globbington
-   /// </summary>
-    public override void Start()
+    //all the controller shit
+    void Start()
     {
-        base.Start();
-
         this.gameObject.name = "Globbington";
-        myRB = this.GetComponent<Rigidbody2D>();
+        myRB = GetComponent<Rigidbody2D>();
         MyPlayerInput.actions.Enable();
         Strike = MyPlayerInput.actions.FindAction("Strike");
 
         Strike.started += Strike_started;
     }
 
-    /// <summary>
-    /// starts strike action (sword swing)
-    /// </summary>
-    /// <param name="obj"></param>
+    //more controller shit
     private void Strike_started(InputAction.CallbackContext obj)
     {
-        Sword.SetActive(true);
+        GlobAttack();
         Invoke("StopAttack", AttackLength);
     }
 
-    /// <summary>
-    /// stops attack - turns sword inactive
-    /// </summary>
+    //attack method !!
+    private void GlobAttack()
+    {
+        Sword.SetActive(true);
+    }
+
+    //knock it off
     private void StopAttack()
     {
         Sword.SetActive(false);
     }
 
-    /// <summary>
-    /// a bunch of math to rotate the sword around globbington
-    /// </summary>
-    private void RotatePlayer()
+    //no more !!
+    private void OnDisable()
     {
-        float angle = Mathf.Atan2(Move.ReadValue<Vector2>().y, Move.ReadValue<Vector2>().x) * Mathf.Rad2Deg;
-        RotatePoint.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    }
-
-    /// <summary>
-    /// moves player and calls rotate for sword
-    /// </summary>
-    /// <returns></returns>
-    public override IEnumerator MovePlayer()
-    {
-        while (ReadMove)
-        {
-            myRB.velocity = Move.ReadValue<Vector2>() * Speed;
-            RotatePlayer();
-            yield return null;
-        }
+        Strike.started -= Strike_started;
     }
 }
