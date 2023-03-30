ecaaaa9729fd994d4ce6d9256133ff1dd0b1a9c8
jayishembryo <kembry@mail.bradley.edu>
2023-03-30 16:41:06 -0500

    commit????

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -8,7 +8,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class DoorScript : MonoBehaviour
 {
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,20 @@
+/*******************************************************************************
+// File Name :         GameManager.cs
+// Author(s) :         Jay Embry
+// Creation Date :     3/30/2023
+//
+// Brief Description : Code that keeps track of game state
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
